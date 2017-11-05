5e457ea3b2209a92d4556234e889b7b7f85d8515
redrize <redri@DESKTOP-NKQQ5SL>
2017-11-05 16:42:48 +0300

    Menu routes addded

---
+++ RSC/Controllers/About.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RSC.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OurTeam()
+        {
+            return View();
+        }
+
+        public IActionResult EkspertniySovet()
+        {
+            return View();
+        }
+        public IActionResult Otdeli()
+        {
+            return View();
+        }
+        public IActionResult Rukovodstvo()
+        {
+            return View();
+        }
+       
+    }
+}
