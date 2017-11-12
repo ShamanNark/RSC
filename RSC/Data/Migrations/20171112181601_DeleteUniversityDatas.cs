d3b390792132b324b2906433f7fa4d8bc44bf8c0
redrize <redri@DESKTOP-NKQQ5SL>
2017-11-12 21:19:21 +0300

    Fixed adding universitydatas table

---
+++ RSC/Data/Migrations/20171112181601_DeleteUniversityDatas.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RSC.Data.Migrations
+{
+    public partial class DeleteUniversityDatas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UniversityDatas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UniversityDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ApplicationUserId = table.Column<int>(nullable: false),
+                    ApplicationUserId1 = table.Column<string>(nullable: true),
+                    RegionId = table.Column<int>(nullable: false),
+                    UniversityAddress = table.Column<string>(nullable: true),
+                    UniversityName = table.Column<string>(nullable: true),
+                    UniversityShortName = table.Column<string>(nullable: true),
+                    UniversityWebSite = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniversityDatas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniversityDatas_AspNetUsers_ApplicationUserId1",
+                        column: x => x.ApplicationUserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversityDatas_ApplicationUserId1",
+                table: "UniversityDatas",
+                column: "ApplicationUserId1");
+        }
+    }
+}
---
+++ RSC/Data/Migrations/20171112181746_AddUniversityDatasTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RSC.Data.Migrations
+{
+    public partial class AddUniversityDatasTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UniversityDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    ApplicationUserId = table.Column<int>(type: "int4", nullable: false),
+                    ApplicationUserId1 = table.Column<string>(type: "text", nullable: true),
+                    RegionId = table.Column<int>(type: "int4", nullable: false),
+                    UniversityAddress = table.Column<string>(type: "text", nullable: true),
+                    UniversityName = table.Column<string>(type: "text", nullable: true),
+                    UniversityShortName = table.Column<string>(type: "text", nullable: true),
+                    UniversityWebSite = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniversityDatas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UniversityDatas_AspNetUsers_ApplicationUserId1",
+                        column: x => x.ApplicationUserId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniversityDatas_ApplicationUserId1",
+                table: "UniversityDatas",
+                column: "ApplicationUserId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UniversityDatas");
+        }
+    }
+}
