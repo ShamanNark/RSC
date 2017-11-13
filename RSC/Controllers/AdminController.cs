04b943b4a775560bb157334e986fbce00569ca46
ShamanNark <7301878@gmail.com>
2017-11-14 00:18:54 +0300

    Add view for confirmation users

--- RSC/Controllers/AdminController.cs
+++ RSC/Controllers/AdminController.cs
@@ -3,19 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RSC.Data;
+using RSC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RSC.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext db;
+        UserManager<ApplicationUser> _userManager;
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            db = context;
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Сonfirmation()
+        public IActionResult СonfirmationUsers()
         {
-            return View();
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditStatusUser(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            ViewBag.Statuses = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem { Selected = true, Text = "Ожидание", Value = "0"},
+                new SelectListItem { Selected = false, Text = "Потвержденно", Value = "1"},
+                new SelectListItem { Selected = false, Text = "Отказанно", Value = "2"},
+            }, "Value", "Text", 0);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditStatusUser(ApplicationUser user)
+        {
+            var userdb = await _userManager.FindByIdAsync(user.Id);
+            userdb.Status = user.Status;
+            await db.SaveChangesAsync();
+            return RedirectToAction("СonfirmationUsers", "Admin");
         }
     }
 }
