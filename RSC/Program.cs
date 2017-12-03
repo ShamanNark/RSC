b4791c1b7f668b83eae4d0b150e587eeb7335cab
redri <redri@DESKTOP-G8BVLTA>
2017-12-04 01:58:37 +0300

    asdasdsa

--- RSC/Program.cs
+++ RSC/Program.cs
@@ -39,6 +39,7 @@ namespace RSC
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:44305/")
                 .UseStartup<Startup>()
                 .Build();
     }
