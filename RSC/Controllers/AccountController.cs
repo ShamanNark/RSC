58e9110820fbf6449ddf293986cab3b93272cc6a
ShamanNark <7301878@gmail.com>
2017-12-03 23:10:46 +0300

    Added email sender

--- RSC/Controllers/AccountController.cs
+++ RSC/Controllers/AccountController.cs
@@ -272,7 +272,7 @@ namespace RSC.Controllers
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                    _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
--- RSC/Services/EmailSender.cs
+++ RSC/Services/EmailSender.cs
@@ -2,16 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace RSC.Services
 {
+    //Example use
+    //public async Task<IActionResult> SendMessage()
+    //{
+    //    EmailSender emailSender = new EmailSender();
+    //    await emailSender.SendEmailAsync("somemail@mail.ru", "Тема письма", "Тест письма: тест!");
+    //    return RedirectToAction("Index");
+    //}
+
     // This class is used by the application to send email for account confirmation and password reset.
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "yur-kuznocov@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("yur-kuznocov@yandex.ru", "0910kaa65*");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
         }
     }
 }
