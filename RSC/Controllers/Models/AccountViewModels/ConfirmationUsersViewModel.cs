41a1af3152ba1faa6aa59f2d9d84d9fe62dc3b46
redri <redri@DESKTOP-G8BVLTA>
2017-12-03 22:03:31 +0300

    semi changes edit status view

--- RSC/Controllers/Models/AccountViewModels/ConfirmationUsersViewModel.cs
+++ RSC/Controllers/Models/AccountViewModels/ConfirmationUsersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace RSC.Controllers.Models.AccountViewModels
         public string Id { get; set; }            
         public string UserName { get; set; }            
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }           
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Статус")]
         public RSC.Models.ApplicationUserStatus Status { get; set; }
         public string AdditionInfoType { get; set; }
         public AdditionInfo AdditionInfo { get; set; }
