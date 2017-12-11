767cfac953ddf49e6449967e3e63feb85225775f
ShamanNark <redri@DESKTOP-G8BVLTA>
2017-12-12 02:44:35 +0300

    Added translate target indicator

--- RSC/Data/DbModels/TargetIndicator.cs
+++ RSC/Data/DbModels/TargetIndicator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace RSC.Data.DbModels
     public class TargetIndicator
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Единица измерения")]
         public string Unit { get; set; }
+        [Display(Name = "Базовое значение")]
         public int BasicValue { get; set; }
+        [Display(Name = "Плановое значение")]
         public int PlannedValue { get; set; }
 
         public int EventId { get; set; }
